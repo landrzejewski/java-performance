aec75ae4c674984ebd1acd2a2d81f856ec628058
landrzejewski <landrzejewski.poczta@gmail.com>
2024-02-22 16:26:46 +0100

    Project

--- src/main/java/pl/training/performance/reports/ApplicationConfig.java
+++ src/main/java/pl/training/performance/reports/ApplicationConfig.java
@@ -3,11 +3,9 @@ package pl.training.performance.reports;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import pl.training.performance.reports.domain.ReportGenerator;
 import pl.training.performance.reports.ports.DataProvider;
 
-@EnableMongoRepositories
 @EnableJpaRepositories
 @Configuration
 public class ApplicationConfig {
